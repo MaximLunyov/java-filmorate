2f067342905e380406d9648f4f7cf821d5ac9470
Maxim Lunyov <mr.minnimi@yandex.com>
2023-05-26 13:13:40 +0300

    "feat: Первая версия FilmrateApplication"

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
 public class ValidationException extends Exception {
-    public ValidationException() {
 
-    }
     public ValidationException(final String message) {
         super(message);
     }
