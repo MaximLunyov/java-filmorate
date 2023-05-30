aaa7462b538cd298696483490eda47373c378657
Maxim Lunyov <mr.minnimi@yandex.com>
2023-05-30 10:53:33 +0300

    "fix: Исправлена реализация валидации и тесты"

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,7 +6,6 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
