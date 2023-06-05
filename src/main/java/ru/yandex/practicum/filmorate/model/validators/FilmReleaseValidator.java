e8669ee59b50a19f56d28fd3956cd7868303f388
Maxim Lunyov <mr.minnimi@yandex.com>
2023-06-05 13:27:42 +0300

    "fix: Реализация валидации через аннотации"

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/validators/FilmReleaseValidator.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.model.validators;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class FilmReleaseValidator implements ConstraintValidator<FilmRelease, LocalDate> {
+
+    @Override
+    public void initialize(FilmRelease constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
+        LocalDate notBefore = LocalDate.of(1895, 12, 28);
+        return !localDate.isBefore(notBefore);
+    }
+}
